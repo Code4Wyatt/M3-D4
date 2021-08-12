4c108fcfd7f60bbf41798093bfb5ab2fd3be5de3
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-12 21:14:54 +0100

    Updated

--- script.js
+++ script.js
@@ -1,83 +1,104 @@
 const getBookSectionRow = document.querySelector(".book-section-row");
+const shoppingCart = document.querySelector("#cart");
 
 window.onload = () => {
     loadBooks()
 };
 
-let books = []
+let books = [];
 
-let shoppingCart = []
+let shoppingCartList = [];
+
+let filteredBooks = [];
+
 function loadBooks() {
     fetch("https://striveschool-api.herokuapp.com/books")
     .then (response => response.json())
-    .then ( _books => {                                         // targeting each book that comes from the API with _books
+    .then ( (_books) => {                                         // targeting each book that comes from the API with _books
         books = _books;                                          // assigning them to the global variable above let books = []
         displayBooks();                                          // then calling displayBooks funtion to show it in the DOM
     })
     .catch((err) => console.error(err.message));
 }
 
-function displayBooks() {
-    books.forEach( (book) => {
-        getBookSectionRow.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3" id="${book.asin}">
-                 <div class="card mx-1">
-                 <img class="card-img-top" src="${book.img}" alt="Card image cap">
-                 <div class="card-body">
-                    <h5>${book.title}</h5>
-                    <p class="card-text mb-0"><strong>Category: </strong> ${book.category}</p>
-                    <p class="card-text"><strong>Price: </strong> ${book.price}€</p>
-                    <i class="fas fa-cart-plus" onclick="addToCart(${book.asin})"></i>
-                    <i class="fas fa-circle-notch"></i> <button type="button" id="buttons" class="btn btn-primary" onclick="addToCartButton()">Add To Cart</button>           </div>
-                  </div>
-                 </div>`
-    })
+function displayBooks(_books = books) {
+    getBookSectionRow.innerHTML = "";
+
+    _books.forEach((book) => {
+        getBookSectionRow.innerHTML += `
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+          <div class="card">
+            <img src="${book.img}" class="img-fluid card-img-top" alt="${
+        book.title
+      }">
+            <div class="card-body">
+              <h5 class="card-title">${book.title}</h5>
+              <p class="card-text">${book.category}</p>
+              <button class="btn btn-primary" onclick="addToCart('${String(
+                book.asin
+              )}', this)">$${book.price}</button>
+              <button class="btn btn-warning" onclick="this.closest('.col-12').remove()">
+                Skip me
+              </button>
+            </div>
+          </div>
+        </div>
+      `;
+    });
 }
 
+function addToCart(asin, element) {   // assigning the asin as the value for addToCart
+    console.log(asin)
+     const book = books.find( (book) => book.asin == asin)   // finding only the asin that matches the one passed
+     shoppingCartList.push(book)
+     refreshShoppingCartList()
 
+     console.log(shoppingCartList);
 
-// let books = []; // storing books as a global variable
-// let getButtons = document.querySelector("#buttons"); //target buttons 
+     element.closest(".card").classList.add("selected-btn");
+}
 
+function refreshShoppingCartList() {
+    shoppingCartList.innerHTML = "";
 
-// let getCards = document.querySelector(".cards"); // target cards
+    shoppingCartList.forEach(book => {
+        shoppingCart.innerHTML += `
+        <div class="shopping-item">
+                <div>
+                 ${book.title}
+                </div>
+                <div>
+                 ${book.price}
+                </div>
+                <button class="btn btn-danger" onclick="deleteItem('${String(
+                    book.asin
+                  )}')">Delete </button>
+        </div>
+        `;
+    });
+}
 
-// // getButtons.addEventListener('click', () => getCards.style.backgroundColor = '#337ab7');
+function search(query) {
+    if (query.length < 3) {    // at query length 3 (when 3 digits entered) 
+      filteredBooks = books;   // use filteredBooks below to conver to lower case
+      displayBooks();           // display books
+      return;
+    }
 
-// window.onload = () => {
-//     fetch ("https://striveschool-api.herokuapp.com/books") // fetching API
-//         .then(response => response.json()) // then converting it to JSON format
-//         .then(receivedBooks => {          
-//             books = receivedBooks              //  assigning receivedBooks to books so we can apply for each to it later?
-//             console.log(receivedBooks)
-//             displayBooks(receivedBooks)         // displayBooks function called on window load to call below function and display each item
-//         })
-//         .catch(error =>{                 // catching any errors that happen
-//             console.log(error)          // console logging error
-//         })
-// }
+    filteredBooks = books.filter((book) =>
+      book.title.toLowerCase().includes(query.toLowerCase())
+    );
 
-// function displayBooks(books) {
-//     let getBookSection = document.querySelector(".book-section .row")  // target book-section
-    
-//     books.forEach(book => {
-//         getBookSection.innerHTML +=
-//         `<div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3" id="${book.asin}">
-//         <div class="card mx-1">
-//         <img class="card-img-top" src="${book.img}" alt="Card image cap">
-//         <div class="card-body">
-//             <h5>${book.title}</h5>
-//             <p class="card-text mb-0"><strong>Category: </strong> ${book.category}</p>
-//             <p class="card-text"><strong>Price: </strong> ${book.price}€</p>
-//             <i class="fas fa-cart-plus" onclick="addToCart(${book.asin})"></i>
-//             <i class="fas fa-circle-notch"></i> <button type="button" id="buttons" class="btn btn-primary" onclick="addToCartButton()">Add To Cart</button>           </div>
-//     </div>
-//     </div>`
-//     })
-// }
+    console.log(filteredBooks);
+    displayBooks(filteredBooks);
+  }
 
-// function addToCartButton() {
-//     let applyStyle = document.getElementById("buttons").addEventListener("click", addToCartButton());
-//     document.getElementById("buttons").classList.add("selectedBtn");
+  function deleteItem(asin) {
+    const index = shoppingCartList.findIndex((book) => book.asin === asin);
 
-  
-// }
+    if (index !== -1) {
+      shoppingCartList.splice(index, 1);
+    }
+
+    refreshShoppingCartList();
+  }
